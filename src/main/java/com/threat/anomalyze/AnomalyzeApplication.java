62997af4fda0317ca0cfff634250e99be0f9a728
Souvik <souvikghosh957@gmail.com>
2025-03-16 21:10:15 +0530

    Initial commit

---
+++ src/main/java/com/threat/anomalyze/AnomalyzeApplication.java
@@ -0,0 +1,13 @@
+package com.threat.anomalyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomalyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnomalyzeApplication.class, args);
+	}
+
+}
